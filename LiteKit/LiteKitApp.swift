caf2fe92fc1a84eb5c6d13e2c19c322a634da36d
Alterem <alterem520@gmail.com>
2025-09-07 17:38:39 +0800

    Initial Commit

---
+++ LiteKit/LiteKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiteKitApp.swift
+//  LiteKit
+//
+//  Created by Alterem on 9/7/25.
+//
+
+import SwiftUI
+
+@main
+struct LiteKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
